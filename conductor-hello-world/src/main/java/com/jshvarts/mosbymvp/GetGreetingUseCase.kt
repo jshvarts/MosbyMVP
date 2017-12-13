0bf78d60db501da828e742df9e8b00910ce64450
James Shvarts <jshvarts@gmail.com>
2017-12-12 19:31:54 -0500

    remove .idea folder

--- conductor-hello-world/src/main/java/com/jshvarts/mosbymvp/GetGreetingUseCase.kt
+++ conductor-hello-world/src/main/java/com/jshvarts/mosbymvp/GetGreetingUseCase.kt
@@ -3,9 +3,6 @@ package com.jshvarts.mosbymvp
 import io.reactivex.Single
 import java.util.concurrent.TimeUnit
 
-/**
- * This is a business logic (Domain layer)
- */
 object GetGreetingUseCase {
 
     fun getHelloGreeting(): Single<String> {
