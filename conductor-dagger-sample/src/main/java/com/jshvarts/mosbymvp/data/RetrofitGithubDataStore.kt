872217571b00005983fcdd93795335ab3964b40e
James Shvarts <jshvarts@gmail.com>
2017-12-21 23:11:24 -0500

    change Maybe to Single for search results

--- conductor-dagger-sample/src/main/java/com/jshvarts/mosbymvp/data/RetrofitGithubDataStore.kt
+++ conductor-dagger-sample/src/main/java/com/jshvarts/mosbymvp/data/RetrofitGithubDataStore.kt
@@ -2,12 +2,9 @@ package com.jshvarts.mosbymvp.data
 
 import com.jshvarts.mosbymvp.domain.GitbhubDataStore
 import com.jshvarts.mosbymvp.domain.GithubRepo
-import io.reactivex.Maybe
 import io.reactivex.Single
 
 class RetrofitGithubDataStore(private val githubService: RetrofitGithubService) : GitbhubDataStore {
-    // TODO handle different http status codes including 404 to emit Maybe.empty()
-    override fun loadRepos(login: String): Maybe<List<GithubRepo>> = githubService.repos(login)
-
+    override fun loadRepos(login: String): Single<List<GithubRepo>> = githubService.repos(login)
     override fun loadRepo(login: String, repoName: String): Single<GithubRepo> = githubService.repo(login, repoName)
 }
--- conductor-dagger-sample/src/main/java/com/jshvarts/mosbymvp/data/RetrofitGithubService.kt
+++ conductor-dagger-sample/src/main/java/com/jshvarts/mosbymvp/data/RetrofitGithubService.kt
@@ -1,14 +1,13 @@
 package com.jshvarts.mosbymvp.data
 
 import com.jshvarts.mosbymvp.domain.GithubRepo
-import io.reactivex.Maybe
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface RetrofitGithubService {
     @GET("users/{login}/repos")
-    fun repos(@Path("login") login: String): Maybe<List<GithubRepo>>
+    fun repos(@Path("login") login: String): Single<List<GithubRepo>>
 
     @GET("repos/{login}/{repoName}")
     fun repo(@Path("login") login: String, @Path("repoName") repoName: String): Single<GithubRepo>
